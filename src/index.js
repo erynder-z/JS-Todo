bf5cb554d5478942863ba5e70642a7ec82b1991c
eryner-z <stfn.bgr@gmail.com>
2022-01-05 17:13:07 +0100

    simple responsive sidebar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./normalize.css";
 import "./style.css";
+
+const sidebar = document.getElementById("sidebar");
+const sidebarBtn = document.getElementById("showSide");
+
+const toggleSidebar = (() => {
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("slide");
+    });
+})();
