d3e4780f4757c47fd8a56debc44c7e4a68b530bf
eryner-z <stfn.bgr@gmail.com>
2022-01-05 19:45:41 +0100

    ewn task button in module

---
+++ src/modules/float.js
@@ -0,0 +1,40 @@
+const floatMenu = () => {
+    let toggleIsHidden = true;
+    const toggleFloatToTop = document.getElementById("myfloat-new-project");
+    const toggleFloatContact = document.getElementById("myfloat-new-task");
+    const toggleFloat = document.getElementById("myfloat");
+
+    toggleFloat.addEventListener("click", () => {
+        handleToogle();
+    });
+
+    toggleFloatToTop.addEventListener("click", () => {
+        handleToogle();
+    });
+
+    toggleFloatContact.addEventListener("click", () => {
+        handleToogle();
+    });
+
+    function showToggle() {
+        toggleFloatToTop.classList.remove("hidden");
+        toggleFloatContact.classList.remove("hidden");
+    }
+
+    function hideToggle() {
+        toggleFloatToTop.classList.add("hidden");
+        toggleFloatContact.classList.add("hidden");
+    }
+
+    function handleToogle() {
+        if (toggleIsHidden == true) {
+            showToggle();
+            toggleIsHidden = false;
+        } else {
+            hideToggle();
+            toggleIsHidden = true;
+        }
+    }
+}
+
+export default floatMenu;
