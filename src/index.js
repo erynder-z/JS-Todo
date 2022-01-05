c4ba43cdf30aecea3564dd2c309e69b9dfc8a942
eryner-z <stfn.bgr@gmail.com>
2022-01-05 18:19:13 +0100

    new task button

--- src/index.js
+++ src/index.js
@@ -6,3 +6,41 @@ import toggleSidebar from "./modules/sidebar"
 const initialize = (() => {
     toggleSidebar();
 })();
+
+
+let toggleIsHidden = true;
+const toggleFloatToTop = document.getElementById("myfloat-new-project");
+const toggleFloatContact = document.getElementById("myfloat-new-task");
+const toggleFloat = document.getElementById("myfloat");
+
+toggleFloat.addEventListener("click", () => {
+    handleToogle();
+});
+
+toggleFloatToTop.addEventListener("click", () => {
+    handleToogle();
+});
+
+toggleFloatContact.addEventListener("click", () => {
+    handleToogle();
+});
+
+function showToggle() {
+    toggleFloatToTop.classList.remove("hidden");
+    toggleFloatContact.classList.remove("hidden");
+}
+
+function hideToggle() {
+    toggleFloatToTop.classList.add("hidden");
+    toggleFloatContact.classList.add("hidden");
+}
+
+function handleToogle() {
+    if (toggleIsHidden == true) {
+        showToggle();
+        toggleIsHidden = false;
+    } else {
+        hideToggle();
+        toggleIsHidden = true;
+    }
+}
