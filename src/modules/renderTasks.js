3a7403578ab4dcd3fe5a3ea503f29b03bcde6b0f
eryner-z <stfn.bgr@gmail.com>
2022-01-07 18:24:18 +0100

    render tasks in own module

---
+++ src/modules/renderTasks.js
@@ -0,0 +1,80 @@
+import {
+    tasks
+} from "./tasks";
+
+const renderTasks = () => {
+
+
+const contentDiv = document.getElementById("mainContent");
+
+const taskListContainer = document.createElement("div");
+
+taskListContainer.classList.add("taskList-container");
+contentDiv.appendChild(taskListContainer);
+
+
+
+tasks.forEach(task => {
+    const taskContainer = document.createElement("div");
+    taskContainer.classList.add("task-container");
+    taskListContainer.appendChild(taskContainer);
+
+    const deleteBtn = document.createElement("div");
+    deleteBtn.classList.add("delete-button");
+    deleteBtn.textContent = "X";
+    taskContainer.appendChild(deleteBtn);
+
+
+    const taskTitleHeading = document.createElement("div");
+    taskTitleHeading.classList.add("task-title-heading");
+    taskTitleHeading.textContent = "Title:";
+    taskContainer.appendChild(taskTitleHeading);
+
+    const taskTitle = document.createElement("div");
+    taskTitle.classList.add("task-title");
+    taskTitle.textContent = task.title;
+    taskContainer.appendChild(taskTitle);
+
+    const taskDescriptionHeading = document.createElement("div");
+    taskDescriptionHeading.classList.add("task-description");
+    taskDescriptionHeading.textContent = "Description:";
+    taskContainer.appendChild(taskDescriptionHeading);
+
+    const taskDescription = document.createElement("div");
+    taskDescription.classList.add("task-description");
+    taskDescription.textContent = task.description;
+    taskContainer.appendChild(taskDescription);
+
+    const dueDateHeading = document.createElement("div");
+    dueDateHeading.classList.add("dueDateHeading");
+    dueDateHeading.textContent = "Due:";
+    taskContainer.appendChild(dueDateHeading);
+
+    const dueDate = document.createElement("div");
+    dueDate.classList.add("dueDate");
+    dueDate.textContent = task.dueDate;
+    taskContainer.appendChild(dueDate);
+
+    const priorityHeading = document.createElement("div");
+    priorityHeading.classList.add("priority-heading");
+    priorityHeading.textContent = "Priority:";
+    taskContainer.appendChild(priorityHeading);
+
+    const priority = document.createElement("div");
+    priority.classList.add("priority");
+    priority.textContent = task.priority;
+    taskContainer.appendChild(priority);
+
+    const notesHeading = document.createElement("div");
+    notesHeading.classList.add("notes-heading");
+    notesHeading.textContent = "Notes:";
+    taskContainer.appendChild(notesHeading);
+
+    const notes = document.createElement("div");
+    notes.classList.add("priority");
+    notes.textContent = task.notes;
+    taskContainer.appendChild(notes);
+});
+}
+
+export default renderTasks
--- src/modules/showAllTasks.js
+++ src/modules/showAllTasks.js
@@ -1,75 +1,25 @@
+import clearContent from "./clearContent";
+import renderTasks from "./renderTasks";
 import {
     tasks
 } from "./tasks";
 
+const contentDiv = document.getElementById("mainContent");
 
 const showAllTasks = () => {
+    clearContent();
 
-    const contentDiv = document.getElementById("mainContent");
+    const headingContainer = document.createElement("div");
+    headingContainer.id = "headingContainer"
+    headingContainer.classList.add("heading-container");
+    contentDiv.appendChild(headingContainer);
 
-
-    let heading = document.createElement("h1");
-    heading.classList.add("allTasks-heading");
+    const heading = document.createElement("h1");
+    heading.classList.add("heading");
     heading.textContent = "All Tasks";
+    headingContainer.appendChild(heading);
 
-    contentDiv.appendChild(heading);
-
-    let taskContainer = document.createElement("div");
-    taskContainer.classList.add("task-container");
-
-    let deleteBtn = document.createElement("div");
-    deleteBtn.classList.add("delete-button");
-
-    let taskTitleHeading = document.createElement("div");
-    taskTitleHeading.classList.add("task-title-heading");
-    let taskTitle = document.createElement("div");
-    taskTitle.classList.add("task-title");
-
-    let taskDescriptionHeading = document.createElement("div");
-    taskDescriptionHeading.classList.add("task-description");
-    let taskDescription = document.createElement("div");
-    taskDescription.classList.add("task-description");
-
-    let dueDateHeading = document.createElement("div");
-    dueDateHeading.classList.add("dueDateHeading");
-    let dueDate = document.createElement("div");
-    dueDate.classList.add("dueDate");
-
-    let priorityHeading = document.createElement("div");
-    priorityHeading.classList.add("priority-heading");
-    let priority = document.createElement("div");
-    priority.classList.add("priority");
-
-    let notesHeading = document.createElement("div");
-    notesHeading.classList.add("notes-heading");
-    let notes = document.createElement("div");
-    notes.classList.add("priority");
-
-
-    for (let i = 0; i < tasks.length; i++) {
-        deleteBtn.textContent = "X";
-        taskTitleHeading.textContent = "Title:";
-        taskTitle.textContent = tasks[i].title;
-        taskDescriptionHeading.textContent = "Description:";
-        taskDescription.textContent = tasks[i].description;
-        dueDateHeading.textContent = "Due:";
-        dueDate.textContent = tasks[i].dueDate;
-        priorityHeading.textContent = "Priority:";
-        priority.textContent = tasks[i].priority;
-
-        contentDiv.appendChild(taskContainer);
-        taskContainer.setAttribute("id", tasks.indexOf(tasks[i]));
-        taskContainer.appendChild(deleteBtn);
-        taskContainer.appendChild(taskTitleHeading);
-        taskContainer.appendChild(taskTitle);
-        taskContainer.appendChild(taskDescriptionHeading);
-        taskContainer.appendChild(taskDescription);
-        taskContainer.appendChild(dueDateHeading);
-        taskContainer.appendChild(dueDate);
-        taskContainer.appendChild(priorityHeading);
-        taskContainer.appendChild(priority);
-    }
-
+    renderTasks(tasks);
 }
 
 export default showAllTasks
