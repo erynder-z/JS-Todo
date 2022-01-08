7018dd606a9a9acfbd54d3f523da80b5a1bfb6e1
eryner-z <stfn.bgr@gmail.com>
2022-01-08 17:36:11 +0100

    show dummy tasks on start

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import "./normalize.css";
 import "./style.css";
 import toggleSidebar from "./modules/sidebar";
 import floatMenu from "./modules/float";
-import createTask from "./modules/tasks";
-import { tasks } from "./modules/tasks";
+import { tasks, createTask } from "./modules/tasks";
 import {createModal, toggleModal} from "./modules/modal";
 import showAllTasks from "./modules/showAllTasks";
+
 
 
 toggleSidebar();
 floatMenu();
 createModal();
+
 showAllTasks();
 
 export default tasks
