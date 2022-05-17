251f8b6e67dcc0897adcf5090b4bd4a2fea20f15
erynder-z <stfn.bgr@gmail.com>
2022-05-17 21:00:53 +0200

    Add  email/password user authentication

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./normalize.css";
 import "./style.css";
+
+import "regenerator-runtime/runtime";
 import floatMenu from "./modules/float";
 import { createNewTaskModal } from "./modules/modal";
 import { activateNav, activateSidebar } from "./modules/buttons";
@@ -7,8 +9,9 @@ import { toggleSidebarNav } from "./modules/sidebar";
 import { retrieveStorage, retrieveStorageCategories } from "./modules/storage";
 import welcomeScreen from "./modules/welcome";
 import switchTheme from "./modules/darkmode";
+import firebaseAuthentication from "./modules/authentication";
 
-const startApp = (() => {
+const startOffline = () => {
   retrieveStorage();
   retrieveStorageCategories();
   activateNav();
@@ -18,4 +21,28 @@ const startApp = (() => {
   createNewTaskModal();
   switchTheme();
   welcomeScreen();
+};
+
+const startOnline = () => {
+  activateNav();
+  toggleSidebarNav();
+  activateSidebar();
+  floatMenu();
+  createNewTaskModal();
+  switchTheme();
+  welcomeScreen();
+};
+
+const loginModal = document.querySelector(".login-modal-overlay");
+const offlineBtn = document.querySelector(".offlineBtn");
+
+offlineBtn.addEventListener("click", () => {
+  loginModal.classList.add("hidden");
+  startOffline();
+});
+
+const start = (() => {
+  firebaseAuthentication();
 })();
+
+export { startOnline };
---
+++ src/modules/authentication.js
@@ -0,0 +1,125 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator,
+  AuthErrorCodes,
+} from "firebase/auth";
+import { startOnline } from "..";
+import { clearMainContent } from "./clearContent";
+
+const firebaseAuthentication = () => {
+  const txtEmail = document.querySelector("#txtEmail");
+  const txtPassword = document.querySelector("#txtPassword");
+  const btnLogin = document.querySelector("#btnLogin");
+  const btnSignup = document.querySelector("#btnSignup");
+  const btnLogout = document.querySelector("#btnLogout");
+  const lblAuthState = document.querySelector("#lblAuthState");
+  const divLoginError = document.querySelector("#divLoginError");
+  const lblLoginErrorMessage = document.querySelector("#lblLoginErrorMessage");
+  const loginModal = document.querySelector(".login-modal-overlay");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAl8gtz0i_XnGFuN5GSi4PC3gG8mrm1O1Q",
+    authDomain: "js-todo-23ba4.firebaseapp.com",
+    projectId: "js-todo-23ba4",
+    storageBucket: "js-todo-23ba4.appspot.com",
+    messagingSenderId: "836500768142",
+    appId: "1:836500768142:web:22e2ba5b6092fd3a7bd0df",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  connectAuthEmulator(auth, "http://localhost:9099");
+
+  const showLoginError = (error) => {
+    divLoginError.style.display = "block";
+    if (error.code === AuthErrorCodes.INVALID_PASSWORD) {
+      lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`;
+    } else {
+      lblLoginErrorMessage.innerHTML = `Error: ${error.message}`;
+    }
+  };
+
+  const showLoginState = (user) => {
+    lblAuthState.innerHTML = `You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `;
+  };
+
+  const hideLoginError = () => {
+    divLoginError.style.display = "none";
+    lblLoginErrorMessage.innerHTML = "";
+  };
+
+  const showLoginForm = () => {
+    loginModal.classList.remove("hidden");
+  };
+
+  const loginEmailPassword = async () => {
+    const loginEmail = txtEmail.value;
+    const loginPassword = txtPassword.value;
+
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+
+      console.log(userCredentials.user);
+    } catch (error) {
+      console.log(error);
+      showLoginError(error);
+    }
+  };
+  btnLogin.addEventListener("click", loginEmailPassword);
+
+  const createAccount = async () => {
+    const loginEmail = txtEmail.value;
+    const loginPassword = txtPassword.value;
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+
+      console.log(userCredentials.user);
+    } catch (error) {
+      console.log(error);
+      showLoginError(error);
+    }
+  };
+
+  btnSignup.addEventListener("click", createAccount);
+
+  const monitorAuthState = async () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        startOnline();
+        showLoginState(user);
+        loginModal.classList.add("hidden");
+        hideLoginError();
+      } else {
+        clearMainContent();
+        showLoginForm();
+
+        lblAuthState.innerHTML = "You're not signed in!";
+      }
+    });
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
+  btnLogout.addEventListener("click", logout);
+
+  monitorAuthState();
+};
+
+export default firebaseAuthentication;
